6c4d9fe6636776aed2f3a839baf2d075a14dcc5a
anandogs <anandoghose@gmail.com>
2022-05-31 19:58:19 +0530

    fixed deployment error

--- components/Cards/YoutubeVideo.tsx
+++ components/Cards/YoutubeVideo.tsx
@@ -3,37 +3,6 @@ import { FunctionComponent } from "react";
 import * as React from "react";
 
 
-// const hasWindow = typeof window !== 'undefined';
-
-// // // function handleResize() {
-// // //   setWindowWidth(getWindowWidth());
-// // // }
-
-// // function getWindowWidth() {
-// //     const width = hasWindow ? window.innerWidth : 500;
-// //     return {
-// //       width,
-// //     };
-// //   }
-
-// // var [windowWidth, setWindowWidth] = useState(500);
-// let windowWidth = 500
-
-// useEffect(() => {
-//     if (hasWindow) {
-//       windowWidth = window.innerWidth
-//     }
-    
-//   }, [hasWindow]);
-
-
-// Define your component props
-
-function componentDidMount() {
-  windowWidth = window.innerWidth;
-}
-
-let windowWidth='500'
 
 type YoutubeVideoProps = {
   youtubeDetails: {
